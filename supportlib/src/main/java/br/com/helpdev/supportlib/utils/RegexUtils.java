1d40e2c0a5a4f94ecc47cec7dbeeece5aa8e0241
Guilherme Biff Zarelli <gbzarelli@gmail.com>
2016-12-27 15:15:30 -0200

    RegexUtils e Style TextView.MyLayoutProgress

---
+++ supportlib/src/main/java/br/com/helpdev/supportlib/utils/RegexUtils.java
@@ -0,0 +1,11 @@
+package br.com.helpdev.supportlib.utils;
+
+/**
+ * Created by Guilherme Biff Zarelli on 06/12/16.
+ */
+
+public class RegexUtils {
+
+    public static final String REGEX_RENACH_TESTES = "^[a-zA-Z]{2}([1]{4}[0-9]{5})$";
+    public static final String REGEX_NUMEROS_IGUAIS = "^(\\d)\\1*$";
+}
