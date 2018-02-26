59d17711d128e4bb1813e30ff2d18ed10273af88
Guilherme Biff Zarelli <gbzarelli@gmail.com>
2018-02-26 11:28:06 -0300

    Ajustes 2 + Brightness

---
+++ supportlib/src/main/java/br/com/helpdev/supportlib/sistema/brightness/BrightnessUtils.java
@@ -0,0 +1,63 @@
+package br.com.helpdev.supportlib.sistema.brightness;
+
+import android.content.ContentResolver;
+import android.provider.Settings;
+
+/**
+ * Created by gbzarelli on 2/26/18.
+ */
+public class BrightnessUtils {
+
+    private BrightnessUtils() {
+        throw new RuntimeException("No BrightnessUtils");
+    }
+
+    /**
+     * Define modo de brilho automatico.
+     * <uses-permission android:name="android.permission.WRITE_SETTINGS" />
+     *
+     * @param contentResolver
+     * @param enabled
+     */
+    public static void setAutomatic(ContentResolver contentResolver, boolean enabled) {
+        Settings.System.putInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS_MODE, enabled ? Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC : Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+    }
+
+    /**
+     * @param contentResolver
+     * @return -1 = impossivel recuperar ; 0 = desabilitado ; 1 = habilitado
+     */
+    public static int isAutomatic(ContentResolver contentResolver) {
+        try {
+            return Settings.System.getInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS_MODE);
+        } catch (Exception e) {
+        }
+        return -1;
+    }
+
+    /**
+     * <uses-permission android:name="android.permission.WRITE_SETTINGS" />
+     *
+     * @param contentResolver
+     * @param brightness      passar valor entre 1-255
+     */
+    public static void setValue(ContentResolver contentResolver, int brightness) {
+        if (brightness < 1 || brightness > 255) {
+            throw new IllegalArgumentException("brightness not in 1-255");
+        }
+        Settings.System.putInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS, brightness);  //brightness is an integer variable (1-255), but dont use 0
+    }
+
+    /**
+     * @param contentResolver
+     * @return -1 = impossivel recuperar; valor de retorno: 0-255
+     */
+    public static int getValue(ContentResolver contentResolver) {
+        try {
+            return Settings.System.getInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS);  //returns integer value 0-255
+        } catch (Exception e) {
+        }
+        return -1;
+    }
+
+}
---
+++ supportlib/src/main/java/br/com/helpdev/supportlib/xml/ControllerSimpleXML.java
@@ -0,0 +1,28 @@
+package br.com.helpdev.supportlib.xml;
+
+import org.simpleframework.xml.Serializer;
+import org.simpleframework.xml.core.Persister;
+
+import java.io.StringWriter;
+
+/**
+ * Created by Guilherme Biff Zarelli on 11/29/17.
+ */
+
+public class ControllerSimpleXML {
+    private ControllerSimpleXML() {
+        throw new RuntimeException("No ControllerSimpleXML");
+    }
+
+    public static <T> T xmlToObject(Class tClass, String xml) throws Exception {
+        Serializer serializer = new Persister();
+        return (T) serializer.read(tClass, xml);
+    }
+
+    public static String objectToXml(Object object) throws Exception {
+        Serializer serializer = new Persister();
+        StringWriter sw = new StringWriter();
+        serializer.write(object, sw);
+        return sw.toString();
+    }
+}
